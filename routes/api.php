f18a557c4bed96990caa5f23e596ae16778f0921
adisunjana <adisunjana443@gmail.com>
2025-01-18 12:20:33 +0700

    register api

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('register', [AuthController::class, 'registerCustomer']);
+Route::post('register/admin', [AuthController::class, 'registerAdmin']);
 
 Route::get('/cars', [CarController::class , 'index']);
 Route::get('/cars/search', [CarController::class, 'search']);
