3a8ab11afdcf001041c8b1adbeb41548a6fd7d5a
adisunjana <adisunjana443@gmail.com>
2025-01-18 10:36:30 +0700

    update car model

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Car extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'brand',
+        'model',
+        'license_plate',
+        'rental_price_per_day',
+        'is_available',
+    ];
 }
