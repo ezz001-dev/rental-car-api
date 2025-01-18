fcaf7731d5eadda32503bd4b5f8d8d78e45cfec1
adisunjana <adisunjana443@gmail.com>
2025-01-18 10:59:39 +0700

    update rentalModel

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Rental extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'car_id',
+        'start_date',
+        'end_date',
+        'return_date',
+        'total_days',
+        'total_cost',
+    ];
+
+    public function car()
+    {
+        return $this->belongsTo(Car::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
