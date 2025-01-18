0af747513b7c0d08eea23eace171f42068b11a7d
adisunjana <adisunjana443@gmail.com>
2025-01-18 10:36:39 +0700

    api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CarController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/cars', [CarController::class , 'index']);
