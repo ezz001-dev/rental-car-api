2c4a68f08bc9f7693e60ecfadda1ffc8e1a0129d
adisunjana <adisunjana443@gmail.com>
2025-01-18 16:13:30 +0700

    update routes api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RentalController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,6 +14,19 @@ Route::get('/user', function (Request $request) {
 
 Route::post('register', [AuthController::class, 'registerCustomer']);
 Route::post('register/admin', [AuthController::class, 'registerAdmin']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::get('/cars', [CarController::class , 'index']);
 Route::get('/cars/search', [CarController::class, 'search']);
+
+Route::post('/cars/add', [CarController::class, 'store'])->middleware(['auth:sanctum' , 'admin']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Peminjaman mobil
+    Route::post('rentals', [RentalController::class, 'rentCar']);
+
+    // Daftar mobil yang sedang disewa oleh pengguna
+    Route::get('rentals/user', [RentalController::class, 'userRentals']);
+});
