95c046378867fcdfb56a16d601fc3c6f5d6879b9
adisunjana <adisunjana443@gmail.com>
2025-01-19 18:19:17 +0700

    add cors middleware

--- routes/api.php
+++ routes/api.php
@@ -12,32 +12,35 @@ use App\Http\Controllers\ReturnRentalController as ReturnController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+Route::middleware(['cors'])->group(function(){
 
-Route::post('register', [AuthController::class, 'registerCustomer']);
-Route::post('register/admin', [AuthController::class, 'registerAdmin']);
-Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::post('register', [AuthController::class, 'registerCustomer']);
+    Route::post('register/admin', [AuthController::class, 'registerAdmin']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::get('/cars', [CarController::class , 'index']);
-Route::get('/cars/search', [CarController::class, 'search']);
+    Route::get('/cars', [CarController::class , 'index']);
+    Route::get('/cars/search', [CarController::class, 'search']);
 
-Route::post('/cars/add', [CarController::class, 'store'])->middleware(['auth:sanctum' , 'admin']);
-
-
-Route::middleware('auth:sanctum')->group(function () {
-
-    // Peminjaman mobil
-    Route::post('rentals', [RentalController::class, 'rentCar']);
-
-    // Daftar mobil yang sedang disewa oleh pengguna
-    Route::get('rentals/user', [RentalController::class, 'userRentals']);
-});
+    Route::post('/cars/add', [CarController::class, 'store'])->middleware(['auth:sanctum' , 'admin']);
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    // Pengembalian mobil
-    Route::post('returns', [ReturnController::class, 'returnCar']);
+    Route::middleware('auth:sanctum')->group(function () {
 
-    // Daftar pengembalian mobil oleh pengguna
-    Route::get('returns/user', [ReturnController::class, 'userReturns']);
+        // Peminjaman mobil
+        Route::post('rentals', [RentalController::class, 'rentCar']);
+
+        // Daftar mobil yang sedang disewa oleh pengguna
+        Route::get('rentals/user', [RentalController::class, 'userRentals']);
+    });
+
+
+    Route::middleware('auth:sanctum')->group(function () {
+        // Pengembalian mobil
+        Route::post('returns', [ReturnController::class, 'returnCar']);
+
+        // Daftar pengembalian mobil oleh pengguna
+        Route::get('returns/user', [ReturnController::class, 'userReturns']);
+    });
+
 });
